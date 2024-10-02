9434a071135c4a32be8b2710e3c4a949f0470483
marcos-vlima <marcosvrl.mv@gmail.com>
2024-10-01 22:08:28 -0300

    Criado o cadastro de produto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
@@ -12,6 +13,20 @@ app.mount("/static", StaticFiles(directory="./static"), name="static")
 @app.get("/")
 def exibir_home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/cadastro")
+def get_contato(request: Request):
+    return templates.TemplateResponse("cadastro.html", {"request": request})
+
+@app.post("/post_cadastro")
+def post_contato(
+    request: Request, 
+    nome: str = Form(...), 
+    descricao: str = Form(...), 
+    estoque: int = Form(...), 
+    preco: int = Form(...),
+    categoria: int = Form(...)):
+    return RedirectResponse("/", 303)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8787, reload=True)
